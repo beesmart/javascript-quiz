378c44b881b3417b910b6ea98628f3bdf36d43b9
beesmart <4colls15@gmail.com>
2014-05-28 09:28:37 +0100

    final draft - working quiz and minor styling

--- script.js
+++ script.js
@@ -18,59 +18,101 @@
 
 
     // setting up some vars
-
     var userChoice = [];
     var div = $('#quizDiv');
     var nButton = $('#nButton');
+    var questionHeading = $("h2");
     var currentQuestion = 0;
-    var txt = "";
+    var score = 0;
 
 
-    // display the 1st question
+
+    // display the questions
     function showQuestion(){
     var displayQuestion = allQuestions[currentQuestion].question;
     var txt = document.createTextNode(displayQuestion);
-        $("h2").append(txt);
+    var questionNo = document.createTextNode("Question " + (currentQuestion+1) );
+    var br = document.createElement('br');
+        questionHeading.append(questionNo);
+        questionHeading.append(br);
+        questionHeading.append(txt);
 
     }
 
+    // display the choices
+    function showChoices() {
+        var displayChoices = allQuestions[currentQuestion].choices;
+        for (var i = 0; i < displayChoices.length; i++) {
+            var lb = document.createElement('label');
+            var inp = document.createElement('input');
+            var br = document.createElement('br');
 
-    // display the first choices
-    var displayChoices = allQuestions[0].choices;
-    for(var i =0; i < displayChoices.length; i ++){
-        var lb = document.createElement('label');
-        var inp = document.createElement('input');
+            inp.setAttribute('type', 'radio');
+            inp.setAttribute('name', 'answer');
+            inp.setAttribute('value', i);
+            lb.appendChild(inp);
+            lb.appendChild(document.createTextNode(displayChoices[i]));
+            div.append(lb);
+            div.append(br);
 
-        inp.setAttribute('type', 'radio');
-        inp.setAttribute('name', 'answer');
-        inp.setAttribute('value', i);
-        lb.appendChild(inp);
-        lb.appendChild(document.createTextNode(displayChoices[i]));
-        div.append(lb);
 
         }
-        div.append(lb); //not sure why I have to repeat this?
+    }
 
     // the button event
-    nButton.on('click', function(){
+    nButton.on('click', function() {
+        if ($('input[name=answer]:checked', '#quizDiv').length === 0){alert("please select an answer");} //if the user has forgotten to choose..
+        else {
+        currentAnswer = ($('input[name=answer]:checked', '#quizDiv').val()); // grab the users choice
+        userChoice.push(currentAnswer); // push it to the array
+        if (userChoice[currentQuestion] == allQuestions[currentQuestion].correctAnswer){score++;} //compare choice and correct answer
 
-    if (currentQuestion <= allQuestions.length){
-        currentQuestion++;
-        $( "h2" ).empty();
-        showQuestion(currentQuestion);
-        console.log(allQuestions.length);
+        currentQuestion++;}
 
+        // clicking onwards
+        if (currentQuestion < allQuestions.length) {
+            questionHeading.empty();
+            div.empty();
+            showQuestion();
+            showChoices();
 
-    }   else { document.getElementById("nButton").disabled = true; }
+     } // finish and disable button
+         else {
+                questionHeading.remove(); // clear the page
+                div.remove();
+                nButton.remove();// disable the button
+                percentScore = Math.floor(((score) / (allQuestions.length))* 100); // get a % score
+                // create the score page and give the user a chance to reload
+                 $("<p>", {
+                      id: 'finalScore', text: 'Your final score is '
+                         })
+                        .appendTo("body")
+                        .append(score)
+                        .append(" out of ")
+                        .append(allQuestions.length);
 
-        console.log(currentQuestion);
+                 $("<h3>", {
+                      id: 'finalPercent', text: "That's "
+                         })
+                        .appendTo("body")
+                        .append(percentScore + "%");
 
-        // log the selected choice
-      alert($('input[name=answer]:checked', '#quizDiv').val())
+                 $("<button>", {
+                      id: 'reloading', name: 'reloading', value: 'reloading', text: "Play again"
+                         })
+                         .appendTo("body")
+                         .on('click', function() {
+                               location.reload();
+                          });
+
+    }
 
      });
 
-showQuestion(currentQuestion);
+showQuestion();
+showChoices();
+
+
 
 
 
